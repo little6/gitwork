b8a818aa4e1095556ded47ee3a9d4d48e1cc522e
little6 <562277204@qq.com>
2018-11-25 12:15:56 +0800

    这是我的git学生用的仓库,我的第一次提交

---
+++ java/cn/itcast/git/GitDemo.java
@@ -0,0 +1,15 @@
+package cn.itcast.git;
+
+/**
+ * @author huyy
+ * @Title: GitDemo
+ * @ProjectName gitwork
+ * @Description: TODO
+ * @date 2018/11/2512:11
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+//        System.out.println("演示git使用的demo");
+    }
+}
